0586479406fb539e7b36a8f26ddfb051260b72db
Vishal Sohaliya <132877214+vills25@users.noreply.github.com>
2025-06-10 21:30:56 +0530

    api integrated

---
+++ APIs/DjangoLoginRegAPI/myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            user_serializer = UserSerializer(user)
+            return Response({
+                'refresh' : str(refresh),
+                'access' : str(refresh.access_token),
+                'user' : user_serializer.data
+            })
+        else:
+            return Response({'detail' : 'Invaalid credentials'}, status = 401)
+       
+
